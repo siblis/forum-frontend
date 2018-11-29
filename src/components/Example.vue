17f1852eeafef39405e3ad192a08ad9dfc534642
andrey <jamesscreen@yandex.ru>
2018-11-29 10:33:33 +0300

    Добавлен футер

--- src/components/Example.vue
+++ src/components/Example.vue
@@ -7,6 +7,7 @@
       <router-link to="/forum-list">Перейти к ForumList</router-link>
       <router-link to="/registration">Перейти к Registration</router-link>
       <router-link to="/login">Перейти к LogIn</router-link>
+      <router-link to="/footer-forum">Перейти к FooterForum</router-link>
     </div>
     <router-view></router-view>
   </div>
